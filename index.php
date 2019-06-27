c4df2eaf6dcf438ae548f73395cac3e425c7da0d
Prince Zezo <princezezo2012@gmail.com>
2019-06-27 09:57:34 +0200

    commit

--- index.php
+++ index.php
@@ -2,8 +2,18 @@
 <html>
 <head>
 	<title>My Online Shop</title>
+	<link rel="stylesheet" href="styles/style.css" media="all">
 </head>
 <body>
-	<h1>Hello World!</h1>
+	<div class="main_wrapper">
+		<div class="header_wrapper">
+			<img id="banner" src="images/penit-Logo-design.gif" alt="ad-banner">
+			<img id="logo" src="images/flash-light-box-300x300.gif" alt="logo">
+		</div>
+		<div>This is menuebar</div>
+		<div>This is sidebar</div>
+		<div>This is content area</div>
+		<div>This is footer</div>
+	</div>
 </body>
 </html>
